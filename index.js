4dca594526644b8e86f684bd62d4f8f2f4780083
rickyricky007 <rickymevrick4@gmail.com>
2026-04-08 08:27:56 +0530

    fix: move db credentials to .env

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 const port = 3000;
@@ -10,11 +12,11 @@ app.use (express.static("public"));
 app.set ("view engine" , "ejs");
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "book",
-    password: "ricky123",
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
 
 db.connect();
